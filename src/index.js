8eca1ff6ede5f6e3e7b1dc52ef477543316c245b
Pavlo Yavorskyi <124560323+ilesyk@users.noreply.github.com>
2024-03-10 14:04:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,11 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/car-rental">
+        <BrowserRouter basename="/learnlingo-app">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-//  basename = '/car-rental';
